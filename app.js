78cbafe45cdda2ef50f485c5d7d542e70a667983
Ian Cover <iancover@gmail.com>
2022-01-21 08:16:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
+// *** NEED YOUTUBE API_KEY ***
 
+const THINKFULTUBE_SEARCH_URL =
+  'https://www.googleapis.com/youtube/v3/search';
 
-var THINKFULTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'eNTerAPiKEyHereOrThISdoEsNTwoRK';
 
-var RESULT_HTML_TEMPLATE = (
+const RESULT_HTML_TEMPLATE =
   '<div class="result-box">' +
-    '<a class="js-result-video" href="" target="_blank"><img class="js-result-thumbnail" src=""></a>' +
-    '<p class="result-title"><a class="js-result-video" href="" target="_blank"><span class="js-result-title"></span></a></p>' +
+  '<a class="js-result-video" href="" target="_blank"><img class="js-result-thumbnail" src=""></a>' +
+  '<p class="result-title"><a class="js-result-video" href="" target="_blank"><span class="js-result-title"></span></a></p>' +
   '</div>' +
-  '<br>'
-);
+  '<br>';
 
 function getDataFromApi(searchTerm, callback) {
-  var query = {
+  const query = {
     part: 'snippet',
-    key: 'Enter_YouTube_API_Key_here',
-    q: searchTerm
-  } 
-    console.log('getDataFromApi running')
-  console.log(query);  
+    key: API_KEY,
+    q: searchTerm,
+  };
+  console.log('getDataFromApi');
+  console.log(query);
   $.getJSON(THINKFULTUBE_SEARCH_URL, query, callback);
 }
 
 function renderResult(result) {
-  var template = $(RESULT_HTML_TEMPLATE);
-  
-  var thumbURL = result.snippet.thumbnails.medium.url
-  var videoTitle = result.snippet.title;
-  var videoURL = "https://www.youtube.com/watch?v=" + result.id.videoId;
+  const template = $(RESULT_HTML_TEMPLATE);
+  const thumbURL = result.snippet.thumbnails.medium.url;
+  const videoTitle = result.snippet.title;
+  const videoURL = 'https://www.youtube.com/watch?v=' + result.id.videoId;
 
-  template.find(".js-result-thumbnail").attr("src", thumbURL);
-  template.find(".js-result-title").text(videoTitle);
-  template.find(".js-result-video").attr("href", videoURL);
-  
-    console.log(videoURL);
+  template.find('.js-result-thumbnail').attr('src', thumbURL);
+  template.find('.js-result-title').text(videoTitle);
+  template.find('.js-result-video').attr('href', videoURL);
+
+  console.log(videoURL);
   console.log('renderResult running');
   return template;
 }
 
 function displayYoutubeSearchData(data) {
-  var results = data.items.map(function(item, index) { // note: .map(fn(item,index)) - reads object as array
-	    console.log('displayYoutubeSearchData running');
+  const results = data.items.map(function (item, index) {
+    console.log('displayYoutubeSearchData running');
     console.log(data);
     return renderResult(item);
   });
   $('.js-search-results').html(results);
 }
-// note: 'data' is the json api script that comes back
 
 function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
+  $('.js-search-form').submit(function (event) {
     event.preventDefault();
-    var queryTarget = $(event.currentTarget).find('.js-searchbox');
-    var querySearch = queryTarget.val(); // extracts value from <input> searchbox
-    queryTarget.val(""); // clear out input also check $('.js-search-form').reset()
+    const queryTarget = $(event.currentTarget).find('.js-searchbox');
+    const querySearch = queryTarget.val();
+    queryTarget.val('');
     $('h2').show();
     console.log('watchSubmit running');
     getDataFromApi(querySearch, displayYoutubeSearchData); // make that value query to run getDataFromApi
